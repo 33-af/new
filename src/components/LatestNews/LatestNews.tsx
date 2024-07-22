96f698cf654c3270f90ea817ec48744ea121e3f0
Mykhail <gorgefraggggg@gmail.com>
2024-07-23 02:25:09 +0300

    add rtk-query

--- src/components/LatestNews/LatestNews.tsx
+++ src/components/LatestNews/LatestNews.tsx
@@ -4,14 +4,14 @@
 import styles from './LatestNews.module.css'
 
 import BannersList from '../BannersList/BannersList'
-import { useFetch } from '../../helpers/hooks/useFetch'
-import { getLatestNews } from '../../api/apiNews'
-import { NewsApiResponse } from '../../interfaces/index'
+import { useGetLatestNewsQuery } from '../../store/services/newsApi'
 
 
 // Таким образом, LatestNews является контейнером, который оборачивает BannersList и передает ему необходимые данные 
 const LatestNews = () => {
-  const {data, isLoading} = useFetch <NewsApiResponse, null>(getLatestNews)
+  // const {data, isLoading} = useFetch <NewsApiResponse, null>(getLatestNews)
+  const { data, isLoading } = useGetLatestNewsQuery(null)
+    
   return (
     <section className={styles.section}>
       <BannersList banners={data && data.news} isLoading={isLoading} />
