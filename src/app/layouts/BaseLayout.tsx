5cbd49f071183d436a99ebba05fad17ab693c500
Mykhail <gorgefraggggg@gmail.com>
2024-07-23 22:54:52 +0300

    fixed bags

--- src/app/layouts/BaseLayout.tsx
+++ src/app/layouts/BaseLayout.tsx
@@ -1,6 +1,7 @@
 
 import { useTheme } from "@/app/providers/ThemeProvider";
-import { MainPage } from "@/pages/main/ui";
+import  MainPage from "@/pages/Main/ui/MainPage"
+
 
 import  Header  from "@/widgets/header/ui/Header/Header";
 
