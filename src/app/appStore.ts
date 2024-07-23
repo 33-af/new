791455bdb5c424b06be12a6978ed0aadfb41f68e
Mykhail <gorgefraggggg@gmail.com>
2024-07-23 22:41:16 +0300

    fixed

--- src/app/appStore.ts
+++ src/app/appStore.ts
@@ -5,13 +5,13 @@ import { newsApi } from "@/entitites/news/api/newsApi";
 import { categoriesApi } from "@/entitites/category/api/categoriesApi";
 
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(newsApi.middleware, categoriesApi.middleware),
-  });
-  
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = typeof store.dispatch;
-  
-  export const useAppDispatch: () => AppDispatch = useDispatch;
-  export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(newsApi.middleware, categoriesApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
